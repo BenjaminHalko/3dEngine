b1d61ec7cb6d8c196c9f705c701a1cdc56b23b5a
Benjamin <benjamin@benjaminhalko.dev>
2025-10-19 16:53:28 -0700

    Delete cmake directory

--- cmake/engine_lib.cmake
+++
@@ -1,4 +0,0 @@
-include(${CMAKE_SOURCE_DIR}/Cmake/common.cmake)
-include(${CMAKE_SOURCE_DIR}/Cmake/library.cmake)
-
-include_directories(${CMAKE_SOURCE_DIR}/Framework ${CMAKE_SOURCE_DIR}/Engine ${CMAKE_SOURCE_DIR}/External)
